5933e6eae8857a1961da1c8bae7b8f3aa1010091
Suna Ulus <suna.ulus@gmail.com>
2022-11-08 22:00:25 -0600

    The connection between database and intellij is ok now-

--- coding-events1/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events1/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,26 +1,68 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
+import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    @Autowired
+   private EventRepository eventRepository;
+
     @GetMapping
     public String displayAllEvents(Model model){
-List<String> events = new ArrayList<>();
-events.add("StrangeLoop");
-events.add("SpringOne Platform");
-model.addAttribute("events", events);
+//model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
 return "events/index";
     }
 @GetMapping("create")
     public String renderCreateEventForm(Model model){
+        model.addAttribute("title", "Create Event");
+        model.addAttribute("event", new Event());
+        model.addAttribute("types",(EventType.values()));
         return "events/create";
     }
+
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad Data!");
+            return "events/create";
+        }
+        eventRepository.save(newEvent);
+        //EventData.add(newEvent);
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        //model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", eventRepository.findAll());
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam (required=false) int [] eventIds){
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                eventRepository.deleteById(id);
+                //EventData.remove(id);
+            }
+        }
+        return "redirect:";
+    }
+
 }
